cefa906e10d3f67276338a4de17c74778a0cb517
SuhaylTFaris <chaosluigi92@hotmail.com>
2015-08-27 10:45:12 -0600

    Added and tested Solution 4.

--- bubbleSort.js
+++ bubbleSort.js
@@ -63,4 +63,22 @@
 //  }
 //  return arr;
 // }
+//
+//Fourth Solution: Runs at .008 Slowest of all solutions.
+// function bubbleSort (arr) {
+//   var cached;
+//
+//   for (var x = 0; x < arr.length; x++) {
+//     for (var i = 0; i < arr.length - 1; i++) {
+//       if (arr[i + 1] < arr[i]) {
+//         // swap
+//         cached = arr[i + 1];
+//         arr[i + 1] = arr[i];
+//         arr[i] = cached;
+//         // console.log(arr);
+//       }
+//     }
+//   }
+//   return arr;
+// }
 module.exports = bubbleSort;
